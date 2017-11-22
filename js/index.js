10f9b362e0e2b35f433a2c3ffbbc1d4b18c75cda
reallygreatstuff <jeffreywisard@gmail.com>
2017-11-21 20:52:22 -0500

    finished AJAX repsonse

--- js/index.js
+++ js/index.js
@@ -6,4 +6,40 @@ $(document).ready(function(){
 		autoplaySpeed: 2000,
 	});
 
-});
+	//Get the show release
+	$.ajax({
+	    url: 'data/shoes.json',
+	    type: 'GET'
+	  })
+
+	  .done(displayShoes)
+	  .fail(function(error){
+	    console.log(error);
+	  });
+
+	  function displayShoes(response) {
+	  	var releases = response.releases;
+	  	var html = '';
+	  	$(releases).each(function(i, shoe){
+	  		var name = shoe.name;
+	  		var date = shoe.date;
+	  		var image = shoe.image;
+	  		var price = shoe.price;
+
+	  		html += `<div class="tile">
+				        <img src="`+ image +`" alt="">
+					    <div class="overlay">
+					      <div class="details">
+					        <p class="name">`+ name +`</p>
+					        <p class="date">`+ date +`</p>
+					        <p class="price">`+ price +`</p>
+					      </div>
+					    </div>
+					  </div>`;
+	  });
+
+	  	$('.tiles').html(html);
+
+	  }
+
+  });
